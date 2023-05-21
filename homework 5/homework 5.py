1528683f7991a7a9df27a18b16ca60e0919c9717
vetydog <samvelnagapetian@gmail.com>
2023-05-21 14:36:50 +0200

    homework 5

---
+++ homework 5/homework 5.py
@@ -0,0 +1,8 @@
+import colorama
+colorama.init()
+print(colorama.Fore.RED + "Красный текст")
+print(colorama.Back.YELLOW + "Текст с желтым фоном")
+print(colorama.Style.BRIGHT + "Яркий текст")
+
+
+
