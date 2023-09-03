b8de915f4c8de2876892b671db78ce1e7cbe4c4f
vetydog <samvelnagapetian@gmail.com>
2023-09-03 14:40:40 +0200

    lesson 6

--- lesson 6/main.py
+++ lesson 6/main.py
@@ -1,20 +1,26 @@
 import warnings
 '''
+try:
+    your_code #requests, database, arythmetic proccess... 
+except ...:
+    proc_error/exception
+'''
+'''
 while(True):
- try :
-     digit1 = 10
-     digit2 = int(input("Enter digit : "))
-     #warnings.warn("\nPossible zero division")
-     res = digit1/digit2
-     print(res)
-
- except ZeroDivisionError as zde :
-    print(f" Cudtom process : {zde}")
- except Exception as ex:
-    print(ex)
- y = input("Enter Y for coninue or N for stop")
- if(y.lower() == "n") :
-     break
+    try:
+        digit1 = 10
+        #warnings.warn("\nPossible zero division!\n")
+        digit2 = int(input("Enter digit: "))
+        res = digit1/digit2
+        print(res)
+    except ZeroDivisionError as zde:
+        print(f"Custom proccess: {zde}")
+    except Exception as ex:
+        print(ex)
+    yes = input("Enter Y for continue or N for stop: ")
+    if(yes.lower() == "n"):
+        break
+print("Cycle while has stoped!")
 '''
 from buildingerror import BuildingError
 from validation import Validation
@@ -22,15 +28,12 @@ limit = 10
 amount = int(input("Enter amount: "))
 check = Validation(amount, limit)
 try:
- check.Check()
+    Validation(amount, limit).Check()
+    #check.Check()
 except ZeroDivisionError as zde:
- print(f"ZeroDivisionError: {zde}")
+    print(f"ZeroDivisionError: {zde}")
 except BuildingError as be:
- print(f"Custom error message\n"
-f"{be}")
+    print(f"Custom error message\n"
+          f"{be}")
 except Exception as ex:
- print(ex)
-
-
-
-
+    print(ex)
